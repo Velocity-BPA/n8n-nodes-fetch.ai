98370ad05ea5719556d1f9a0726044f986c88ed6
John Drefahl <john@velobpa.com>
2026-02-24 04:19:58 -0600

    chore: update gulpfile.js to v1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
+ * Licensed under the Business Source License 1.1
+ */
+
 const { src, dest } = require('gulp');
 
-/**
- * Copy node icons to the dist folder during build
- */
 function buildIcons() {
-	return src('nodes/**/*.svg')
-		.pipe(dest('dist/nodes'));
+  return src('nodes/**/*.{png,svg}').pipe(dest('dist/nodes'));
 }
 
 exports['build:icons'] = buildIcons;
